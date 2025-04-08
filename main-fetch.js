deedde4612ab8219d8c5eb487abdbfc7ae3c010f
Felix Lee <fexacademico@gmail.com>
2025-04-08 16:16:33 -0600

    chg: adding new todo list application using APIs

---
+++ main-fetch.js
@@ -0,0 +1,19 @@
+// Copyright © 2025 Felix Lee Pan
+// All rights reserved. Unauthorized copying, modification, or distribution of this software is prohibited.
+
+
+function loadTasks() {
+
+}
+
+function renderTask(task) {
+
+}
+
+function editTask(id) {
+
+}
+
+function deleteTask(id) {
+
+}
---
+++ main-jquery.js
@@ -0,0 +1,12 @@
+// Copyright © 2025 Felix Lee Pan
+// All rights reserved. Unauthorized copying, modification, or distribution of this software is prohibited.
+
+
+
+function loadTasks() {
+
+}
+
+function renderTask(task) {
+
+}
